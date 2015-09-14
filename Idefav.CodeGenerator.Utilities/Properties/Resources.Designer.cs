89f6b9742be34348b53d0ecf0bbd084b1999a3c3
wzs <wzs2009_hi@126.com>
2015-09-14 13:56:11 +0800

    111

--- Idefav.CodeGenerator.Utilities/Properties/Resources.Designer.cs
+++ Idefav.CodeGenerator.Utilities/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Idefav.CodeGenerator.Properties {
+namespace Idefav.CodeGenerator.Utilities.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Idefav.CodeGenerator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Idefav.CodeGenerator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Idefav.CodeGenerator.Utilities.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Idefav.CodeGenerator.Utilities/Properties/Settings.Designer.cs
+++ Idefav.CodeGenerator.Utilities/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Idefav.CodeGenerator.Properties {
+namespace Idefav.CodeGenerator.Utilities.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
